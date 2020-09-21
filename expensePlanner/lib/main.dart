e260708af6fdc713c24caaf1cb49fdd1c1e09e77
Anna-12-Mer <mammeramna@gmail.Com>
2020-09-21 18:12:01 +0200

    create a new project Flutter

---
+++ expensePlanner/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter App'),
+      ),
+      body: Center(child: Text('widget Playground!')),
+    );
+  }
+}
